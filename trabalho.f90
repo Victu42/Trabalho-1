327cb0799475b25c33f2b5b9f7a8c44da796754f
Victor <victor.arthuso@ufv.br>
2024-11-18 12:12:49 -0300

    Adição de Variância e Desvio

--- trabalho.f90
+++ trabalho.f90
@@ -5,12 +5,14 @@ program stats
 
    integer(i8) :: n, f_u, i, io
    real(dp), allocatable :: a(:), b(:)
-   real(dp) :: media_a, media_b, soma_a, soma_b
+   real(dp) :: media_a, media_b, var_a, var_b, dp_a, dp_b
 
    f_u = 11
    n = 0
    media_a = 0
    media_b = 0
+   var_a = 0
+   var_b = 0
 
    open(f_u, file="data.txt", iostat=io, status="OLD", action="read")
 
@@ -37,8 +39,22 @@ program stats
 
    end do 
 
-   write(*,*) media_a 
-   write(*,*) media_b
+   do i=1,n 
+
+   var_a = var_a + ((a(i) - media_a)*2)/(n-1)
+   var_b = var_b + ((b(i) - media_b)*2)/(n-1)
+
+   end do
+
+   dp_a = sqrt(var_a)
+   dp_b = sqrt(var_b)
+
+   write(*,*) 'Média Coluna 1:',media_a 
+   write(*,*) 'Média Coluna 2:',media_b
+   write(*,*) 'Variância Coluna 1:',var_a
+   write(*,*) 'Variância Coluna 2:',var_b
+   write(*,*) 'Desvio Padrão Coluna 1:',dp_a
+   write(*,*) 'Desvio Padrão Coluna 2:',dp_b
 
 end program stats
 
