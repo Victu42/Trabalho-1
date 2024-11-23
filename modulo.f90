c9d604600c657cb999ae64f8a47bf9b0e7658b77
Victor <victor.arthuso@ufv.br>
2024-11-22 22:32:52 -0300

    adição de comentários e documentação

--- modulo.f90
+++ modulo.f90
@@ -9,24 +9,24 @@ character (len = 1) :: input
 
 contains
 
-   function linhas() result(n)
+   function linhas() result(n) !função que conta as linhas
    
       integer(i8):: n
-      n = 0
-      open(f_u, file="data.txt", iostat=io, status="OLD", action="read")
-          do 
+      n = 0 !define o valor inicial de n como 0 para que possa ser somado mais à frente
+      open(f_u, file="data.txt", iostat=io, status="OLD", action="read") !abre o arquivo de dados
+          do !loop para contagem de linhas
        
-             read(f_u, *, iostat=io)
-             if (io /= 0) exit
-             n = n + 1
+             read(f_u, *, iostat=io) !confere se há uma linha
+             if (io /= 0) exit !quebra o loop caso não haja uma linha
+             n = n + 1 !adiciona uma linha à contagem em caso de linha existente
        
           end do 
       
-      close(unit=f_u)
+      close(unit=f_u) !fecha o arquivo
    
    end function linhas
 
-   subroutine define(a, b)
+   subroutine define(a, b) !subrotina que armazena os dados do arquivo em dois vetores
       
       integer(i8) :: i
       real(dp), dimension(n_linhas) :: a(:), b(:)
@@ -35,12 +35,12 @@ contains
 
       open(f_u, file="data.txt", iostat=io, status="OLD", action="read")
    
-      do i=1, n_linhas
+      do i=1, n_linhas !loop para iterar por todo o arquivo e armazenar as linhas
    
-         read(f_u, *, iostat=io) a(i), b(i)
-         col1(i)=a(i)
-         col2(i)=b(i)
-         if (io /= 0) exit
+         read(f_u, *, iostat=io) a(i), b(i) !passa os dados para os vetores
+         col1(i)=a(i) !escreve o item na variável inicial col1
+         col2(i)=b(i) !escreve o item na variável inicial col2
+         if (io /= 0) exit !quebra o loop caso o arquivo acabe
    
       end do 
 
@@ -49,7 +49,7 @@ contains
 
    end subroutine define
 
-   function media(col_m) result(m)
+   function media(col_m) result(m)!calcula a média
 
       integer(i8) :: j
       real(dp) :: m, col_m(:)
@@ -57,59 +57,59 @@ contains
       j = 0
       m = 0
 
-      do j=1, n_linhas
+      do j=1, n_linhas !loop para iterar pelos dados 
 
-         m = m + (col_m(j)/n_linhas)
-         if (io /= 0) exit
+         m = m + (col_m(j)/n_linhas) !adiciona termos à média total
+         if (io /= 0) exit !quebra o loop
    
       end do 
 
    end function media
 
-   function varia(col_v) result(var)
+   function varia(col_v) result(var) !calcula a variância
 
       real(dp) :: var, col_v(:), md
       integer(i8) :: k
 
       var = 0
-      md = media(col_v)
+      md = media(col_v) !chama a função media
 
-      do k=1, n_linhas 
+      do k=1, n_linhas !loop para iterar pelos dados
 
-         var = var + ((col_v(k) - md)*2/(n_linhas-1))
+         var = var + ((col_v(k) - md)*2/(n_linhas-1)) !adiciona termos à variância total
 
       end do
    
    end function varia
 
-   function desvio(col_d) result(dv_p)
+   function desvio(col_d) result(dv_p) !calcula o desvio padrão
    
       real(dp) :: dv_p, col_d(:)
 
-      dv_p = sqrt(varia(col_d))
+      dv_p = sqrt(varia(col_d)) !fórmula para o desvio padrão
 
    end function desvio
 
-   subroutine media_movel
+   subroutine media_movel !calcula a média móvel dos dados
       
       real(dp), dimension(n_linhas) :: mm_1, mm_2
       real(dp) :: calc_1, calc_2
       integer(i8) :: l 
       
-      do
-         print *, 'Gostaria de um arquivo com a média móvel dos seus dados? (Y/N)'
-         read(*,*) input
-         if (input /= "Y" .and. input /= "N") then
-            print *, 'Resposta inválida.'
-         else
+      do !loop para consultar se a pessoa quer a média móvel
+         print *, 'Gostaria de um arquivo com a média móvel dos seus dados? (Y/N)' !faz a pergunta
+         read(*,*) input !lê a resposta
+         if (input /= "Y" .and. input /= "N") then !confere se a resposta corresponde ao aceito pelo programa
+            print *, 'Resposta inválida.' !caso não corresponda, exibe erro 
+         else !caso corresponda, sai do loop
             exit 
          end if
       end do
 
-      if (input == 'Y') then
+      if (input == 'Y') then !se a resposta é sim (Y), calcula a média móvel
          
-         open(f_u, file="data.txt", iostat=io, status="OLD", action="read")
-         open(f_u_2, file="data_media_movel.txt", iostat=io, status="NEW", action="write")
+         open(f_u, file="data.txt", iostat=io, status="OLD", action="read") !abre o arquivo
+         open(f_u_2, file="data_media_movel.txt", iostat=io, status="NEW", action="write") !cria o novo arquivo
          
          mm_1(1) = (col1(1)+col1(2)+col1(3))/3
          mm_1(2) = (col1(1)+col1(2)+col1(3)+col1(4))/4
@@ -119,12 +119,13 @@ contains
          mm_1(n_linhas-1) = (col1(n_linhas)+col1(n_linhas-1)+col1(n_linhas-2)+col1(n_linhas-3))/4
          mm_2(n_linhas) = (col2(n_linhas)+col2(n_linhas-1)+col2(n_linhas-2))/3
          mm_2(n_linhas-1) = (col2(n_linhas)+col2(n_linhas-1)+col2(n_linhas-2)+col2(n_linhas-3))/4
+         !as linhas acima calculam a média móvel para os valores que não possuem amostras nas vizinhanças, dado o range de 5
 
-         do l = 3, n_linhas-2
+         do l = 3, n_linhas-2 !loop que calcula a média móvel para o restante dos dados
 
-            mm_1(l) = (col1(l-2)+col1(l-1)+col1(l)+col1(l+1)+col1(l+2))/5
-            mm_2(l) = (col2(l-2)+col2(l-1)+col2(l)+col2(l+1)+col2(l+2))/5
-            write(f_u_2,*) mm_1(l), mm_2(l)
+            mm_1(l) = (col1(l-2)+col1(l-1)+col1(l)+col1(l+1)+col1(l+2))/5 !tira a média dos valores da vizinhança para a coluna 1
+            mm_2(l) = (col2(l-2)+col2(l-1)+col2(l)+col2(l+1)+col2(l+2))/5 !tira a média dos valores da vizinhança para a coluna 2
+            write(f_u_2,*) mm_1(l), mm_2(l) !escreve os valores no novo arquivo
 
          end do
 
--- trabalho.f90
+++ trabalho.f90
@@ -1,24 +1,24 @@
 program stats
 
-use estatistica_basica
+use estatistica_basica !adiciona o módulo
 implicit none
 
    f_u = 11
    f_u_2 = 12
-   n_linhas = linhas()
+   n_linhas = linhas() !chama a função para contagem de linhas
 
-   allocate(col1(n_linhas), col2(n_linhas))
+   allocate(col1(n_linhas), col2(n_linhas)) !define a dimensão dos vetores de acordo com a quantidade de linhas do arquivo
 
-   call define(col1,col2)
+   call define(col1,col2) !chama a subrotina para definir os vetores
 
-   write(*,*) 'Média Coluna 1:',media(col1)
-   write(*,*) 'Média Coluna 2:',media(col2)
-   write(*,*) 'Variância Coluna 1:',varia(col1)
-   write(*,*) 'Variância Coluna 2:',varia(col2)
-   write(*,*) 'Desvio Padrão Coluna 1:',desvio(col1)
-   write(*,*) 'Desvio Padrão Coluna 2:',desvio(col2)
+   write(*,*) 'Média Coluna 1:',media(col1) !escreve a média da coluna 1
+   write(*,*) 'Média Coluna 2:',media(col2) !escreve a média da coluna 2
+   write(*,*) 'Variância Coluna 1:',varia(col1) !escreve a variância da coluna 1
+   write(*,*) 'Variância Coluna 2:',varia(col2) !escreve a variância da coluna 2
+   write(*,*) 'Desvio Padrão Coluna 1:',desvio(col1) !escreve o desvio da coluna 1
+   write(*,*) 'Desvio Padrão Coluna 2:',desvio(col2) !escreve o desvio da coluna 2
 
-   call media_movel
+   call media_movel !chama a subrotina para cálculo da Média Móvel
 
 end program stats
 
